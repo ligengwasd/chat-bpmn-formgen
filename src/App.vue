9733030ad4551be09c230b87c20d9b701ce4a5bd
geng.li <geng.li@timschina.com>
2023-04-15 22:47:57 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,69 @@
-
-
 <template>
-    <div class="home-TUIKit-main">
-        <div :class="env?.isH5 ? 'conversation-h5' : 'conversation'" v-show="!env?.isH5 || currentModel === 'conversation'">
-            <TUISearch class="search" />
-            <TUIConversation @current="handleCurrentConversation" />
-        </div>
-        <div class="chat" v-show="!env?.isH5 || currentModel === 'message'">
-            <TUIChat>
-                <h1>欢迎使用腾讯云即时通信IM</h1>
-            </TUIChat>
-        </div>
-        <Drag :show="showCall" class="callkit-drag-container" domClassName="callkit-drag-container">
-            <!-- TUICallKit 组件：通话 UI 组件主体 -->
-            <TUICallKit
-                    :allowedMinimized="true"
-                    :allowedFullScreen="false"
-                    :beforeCalling="beforeCalling"
-                    :afterCalling="afterCalling"
-                    :onMinimized="onMinimized"
-                    :onMessageSentByMe="onMessageSentByMe"
-            />
-        </Drag>
-        <Drag :show="showCallMini" class="callkit-drag-container-mini" domClassName="callkit-drag-container-mini">
-            <!-- TUICallKitMini 组件：通话 UI 悬浮窗组件，提供最小化功能 -->
-            <TUICallKitMini style="position: static" />
-        </Drag>
-    </div>
+    <el-container>
+        <el-header>
+            <h1>IM & 智能机器人 & 工作流 缝合怪</h1>
+        </el-header>
+        <el-container>
+            <el-aside width="200px">
+                <el-menu>
+                    <el-menu-item index="/home/chat">
+                        <sss></sss>
+                        <router-link to="/home/chat">即时通信</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="/home/formGenerator">
+                        <router-link to="/home/formGenerator">表单设计器</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="/home/formParser">
+                        <router-link to="/home/formParser">表单解析器</router-link>
+                    </el-menu-item>
+                    <el-menu-item>流程设计器</el-menu-item>
+                </el-menu>
+            </el-aside>
+            <el-container>
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
+            </el-container>
+        </el-container>
+    </el-container>
 </template>
 
-
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from 'vue';
-import { TUIEnv } from './TUIKit/TUIPlugin';
-import Drag from './TUIKit/TUIComponents/components/drag';
-import { handleErrorPrompts } from './TUIKit/TUIComponents/container/utils';
 
-
-
-
-export default defineComponent({
-    name: 'App',
-    components: {
-        Drag,
-    },
-    setup() {
-        const data = reactive({
-            env: TUIEnv(),
-            currentModel: 'conversation',
-            showCall: false,
-            showCallMini: false,
-        });
-        const TUIServer = (window as any)?.TUIKitTUICore?.TUIServer;
-        const handleCurrentConversation = (value: string) => {
-            data.currentModel = value ? 'message' : 'conversation';
-        };
-        // beforeCalling：在拨打电话前与收到通话邀请前执行
-        const beforeCalling = (type: string, error: any) => {
-            if (error) {
-                handleErrorPrompts(error, type);
-                return;
-            }
-            data.showCall = true;
-        };
-        // afterCalling：结束通话后执行
-        const afterCalling = () => {
-            data.showCall = false;
-            data.showCallMini = false;
-        };
-        // onMinimized：组件切换最小化状态时执行
-        const onMinimized = (oldMinimizedStatus: boolean, newMinimizedStatus: boolean) => {
-            data.showCall = !newMinimizedStatus;
-            data.showCallMini = newMinimizedStatus;
-        };
-        // onMessageSentByMe：在整个通话过程内发送消息时执行
-        const onMessageSentByMe = async (message: any) => {
-            TUIServer?.TUIChat?.handleMessageSentByMeToView(message);
-            return;
-        };
-        return {
-            ...toRefs(data),
-            handleCurrentConversation,
-            beforeCalling,
-            afterCalling,
-            onMinimized,
-            onMessageSentByMe,
-        };
-    },
-});
 </script>
 
+<style lang="scss" scoped>
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+}
 
-<style scoped>
-.home-TUIKit-main {
-    display: flex;
-    height: 100vh;
-    overflow: hidden;
+.el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
 }
-.search {
-    padding: 12px;
+
+.el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
 }
-.conversation {
-    min-width: 285px;
-    flex: 0 0 24%;
-    border-right: 1px solid #f4f5f9;
+
+body > .el-container {
+    margin-bottom: 40px;
 }
-.conversation-h5 {
-    flex: 1;
-    border-right: 1px solid #f4f5f9;
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+    line-height: 260px;
 }
-.chat {
-    flex: 1;
-    height: 100%;
-    position: relative;
-}
-.callkit-drag-container {
-    left: calc(50% - 25rem);
-    top: calc(50% - 18rem);
-    width: 50rem;
-    height: 36rem;
-    border-radius: 16px;
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
-}
-.callkit-drag-container-mini {
-    width: 168px;
-    height: 56px;
-    right: 10px;
-    top: 70px;
+
+.el-container:nth-child(7) .el-aside {
+    line-height: 320px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { TUIComponents, TUICore, genTestUserSig } from './TUIKit';
 // import TUICallKit
 import { TUICallKit } from '@tencentcloud/call-uikit-vue';
 import router from './router';
+import ElementPlus from 'element-plus';//为vue3项目特别更新的版本
+import 'element-plus/dist/index.css';
+
 
 
 const SDKAppID = 1400034652; // Your SDKAppID
@@ -35,5 +38,6 @@ TUIKit.login({
 createApp(App)
     .use(TUIKit)
     .use(router)
+    .use(ElementPlus)
     .mount('#app');
 
