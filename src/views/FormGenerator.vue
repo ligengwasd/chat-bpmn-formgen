0d92f42fe1ecf66ee389e1c3a290054788ed68bd
geng.li <geng.li@timschina.com>
2023-04-19 12:15:56 +0800

    bpmn

--- src/views/FormGenerator.vue
+++ src/views/FormGenerator.vue
@@ -1,14 +1,32 @@
 <template>
-    <div>
-        <fc-designer ref="designer"/>
-    </div>
+    <el-row :gutter="20">
+        <el-col :span="6">
+            <el-input v-model="formName" placeholder="请输入表单名称"></el-input>
+        </el-col>
+        <el-col :span="6">
+            <el-button type="primary" @click="exportForm()">创建表单</el-button>
+        </el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <br>
+    <fc-designer ref="designer"/>
+
+
 </template>
 
 <script>
 export default {
     data() {
         return {
+            formName:""
         }
+    },
+    methods: {
+        exportForm() {
+            const json = this.$refs.designer.getRule();
+            console.log(json);
+        },
     }
 }
 </script>
