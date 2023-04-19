22f3a9a599c6923c28446defad430835cf787479
geng.li <geng.li@timschina.com>
2023-04-19 17:00:00 +0800

    bpmn

--- src/views/FormParser.vue
+++ src/views/FormParser.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         <form-create :rule="rule" v-model:api="fApi" :option="options" v-model="value"/>
+        <form-create :rule="httpClient.rule" :option="httpClient.options" />
+
     </div>
 </template>
 
@@ -30,7 +32,71 @@ export default {
                     },
                     children: ['点击'],
                 }
-            ]
+            ],
+            httpMethod:"",
+            httpUrl:"",
+            httpClient:{
+                rule:[
+                    {
+                        "type": "select",
+                        "field": "httpMethod",
+                        "title": "请求方式",
+                        "info": "请求方式",
+                        "props": {
+                            "_optionType": 2
+                        },
+                        "_fc_drag_tag": "select",
+                        "hidden": false,
+                        "display": true,
+                        "options": [
+                            {
+                                "label": "GET",
+                                "value": "GET"
+                            },
+                            {
+                                "label": "POST",
+                                "value": "POST"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "FcRow",
+                        "_fc_drag_tag": "row",
+                        "hidden": false,
+                        "display": true
+                    },
+                    {
+                        "type": "input",
+                        "field": "httpUrl",
+                        "title": "请求地址",
+                        "info": "",
+                        "_fc_drag_tag": "input",
+                        "hidden": false,
+                        "display": true
+                    },
+                    {
+                        "type": "input",
+                        "field": "httpBody",
+                        "title": "请求体",
+                        "info": "",
+                        "_fc_drag_tag": "input",
+                        "hidden": false,
+                        "display": true
+                    }
+                ],
+                options:{
+                    "form": {
+                        "labelPosition": "right",
+                        "size": "medium",
+                        "labelWidth": "125px",
+                        "hideRequiredAsterisk": false,
+                        "showMessage": true,
+                        "inlineMessage": false
+                    },
+                    "submitBtn": false,
+                    "resetBtn": false
+                }
+            }
         }
     }
 }
