a4f7331d4e2237d0266c4f0ca4d82c9986d6340c
geng.li <geng.li@timschina.com>
2023-04-20 20:31:59 +0800

    update

--- src/views/FormGenerator.vue
+++ src/views/FormGenerator.vue
@@ -1,34 +1,34 @@
 <template>
-    <el-row :gutter="20">
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6">
-            <el-select v-model="selectForm" placeholder="请选择" @change="selectChange">
-                <el-option
-                    v-for="item in formList"
-                    :key="item.key"
-                    :label="item.key"
-                    :value="item.value">
-                </el-option>
-            </el-select>
-        </el-col>
-        <el-col :span="6">
-            <el-button type="primary" @click="exportForm()">编辑所选中表单</el-button>
-        </el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-    </el-row>
-    <br>
-    <el-row :gutter="20">
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6">
-            <el-input v-model="formName" placeholder="请输入表单名称"></el-input>
-        </el-col>
-        <el-col :span="6">
-            <el-button type="primary" @click="exportForm()">保存表单</el-button>
-        </el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-    </el-row>
-    <br>
-    <fc-designer ref="designer"/>
+    <el-table :data="formList" border style="width: 100%">
+        <el-table-column prop="key" label="表单名称" width="180" />
+        <el-table-column label="操作" width="180">
+            <template #default="props">
+                <el-button type="primary" @click="showEditFormDialog(props.row)">编辑</el-button>
+            </template>
+        </el-table-column>
+    </el-table>
+<!--    <br>-->
+<!--    <el-row :gutter="20">-->
+<!--        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>-->
+<!--        <el-col :span="6">-->
+<!--            <el-input v-model="formName" placeholder="请输入表单名称"></el-input>-->
+<!--        </el-col>-->
+<!--        <el-col :span="6">-->
+<!--            <el-button type="primary" @click="exportForm()">保存表单</el-button>-->
+<!--        </el-col>-->
+<!--        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>-->
+<!--    </el-row>-->
+
+    <el-dialog v-model="dialogVisible" title="表单设计器" :before-close="handleClose" style="width: 80%; height: 80%">
+        <fc-designer ref="designer"/>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="dialogVisible = false">Cancel</el-button>
+                <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
+            </span>
+        </template>
+    </el-dialog>
+
 
 
 </template>
@@ -41,7 +41,8 @@ export default {
         return {
             formName: "",
             formList: [],
-            selectForm: null
+            selectForm: null,
+            dialogVisible: false
         }
     },
     mounted() {
@@ -59,7 +60,6 @@ export default {
                 "value": JSON.stringify(bizConfigValue)
             }
             console.log(param);
-            // this.$http.post('/task/1/comment/create', param).then(response => {
             this.$http.post('/bizConfig', param).then(response => {
                 console.log(response.data);
             }) .catch(error => {
@@ -72,6 +72,12 @@ export default {
                 this.formList = response.data;
             });
         },
+        showEditFormDialog(rowData) {
+            this.dialogVisible = true;
+            const selectForm = JSON.parse(rowData.value);
+            this.$refs.designer.setRule(selectForm.formRule);
+            this.$refs.designer.setOption(selectForm.formOptions);
+        },
         selectChange() {
             const selectForm = JSON.parse(this.selectForm);
             this.$refs.designer.setRule(selectForm.formRule);
@@ -81,33 +87,4 @@ export default {
 }
 </script>
 <style>
-.time {
-    font-size: 13px;
-    color: #999;
-}
-
-.bottom {
-    margin-top: 13px;
-    line-height: 12px;
-}
-
-.button {
-    padding: 0;
-    float: right;
-}
-
-.image {
-    width: 100%;
-    display: block;
-}
-
-.clearfix:before,
-.clearfix:after {
-    display: table;
-    content: "";
-}
-
-.clearfix:after {
-    clear: both
-}
 </style>
