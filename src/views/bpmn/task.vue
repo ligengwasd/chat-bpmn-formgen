bce39b416d18d3a634a4faccb10d2723089767df
geng.li <geng.li@timschina.com>
2023-04-23 17:27:55 +0800

    update

--- src/views/bpmn/task.vue
+++ src/views/bpmn/task.vue
@@ -1,9 +1,14 @@
 <template>
-    <el-table :data="taskList" border style="width: 100%">
-        <el-table-column prop="id" label="表单名称" width="180" />
-
+    <el-table :data="taskList" border stripe style="width: 100%; height: 800px">
+        <el-table-column prop="id" label="任务ID" width="80" show-overflow-tooltip/>
+        <el-table-column prop="assignee" label="指派人" width="80" />
+        <el-table-column prop="name" label="任务名称" width="120" />
+        <el-table-column prop="executionId" label="执行流ID" width="180" show-overflow-tooltip/>
+        <el-table-column prop="processDefinitionId" label="流程定义ID" width="180" show-overflow-tooltip/>
+        <el-table-column prop="processInstanceId" label="流程实例ID" width="180" show-overflow-tooltip/>
+        <el-table-column prop="taskDefinitionKey" label="taskDefinitionKey" width="180" />
+        <el-table-column prop="created" label="创建时间" width="180" />
     </el-table>
-
 </template>
 
 <script>
@@ -22,9 +27,17 @@ export default {
     },
     methods: {
         loadTaskList() {
-            this.$http.post('/engine-rest/task').then(response => {
-                console.log(response.data);
+            this.$http.get('/engine-rest/task', {
+            }, {
+                params: {
+                }
+            })
+            .then(response => {
                 this.taskList = response.data;
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error(error);
             });
         }
 
