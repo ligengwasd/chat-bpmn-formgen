e91e433d9e00a6e7846a1bd076269f4d3d27853b
geng.li <geng.li@timschina.com>
2023-05-12 20:22:01 +0800

    动态表格

--- src/views/oauth2/login.vue
+++ src/views/oauth2/login.vue
@@ -1,10 +1,26 @@
 <template>
-是是是
+  OAuth-Server登录页
+    <br>
+    <br>
+    <el-button type="primary" @click="doLogin()">点击登录</el-button>
 </template>
 
 <script>
+import { encode, decode } from 'js-base64';
+
 export default {
-    name: "login"
+    name: "login",
+    methods: {
+        doLogin() {
+            console.log(111)
+            let authorizeUrlEncode = this.$route.query.authorizeUrl;
+            let authorizeUrl = decode(authorizeUrlEncode)
+            this.$http.get('http://localhost:8001/oauth2/doLogin?name=sa&pwd=123456').then(response => {
+                console.log(authorizeUrl);
+                // window.location.replace = authorizeUrl;
+            })
+        }
+    }
 }
 </script>
 
