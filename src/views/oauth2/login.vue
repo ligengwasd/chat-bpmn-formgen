97890ff10e55ed0665c9d79bdc48342d4b4ab280
geng.li <geng.li@timschina.com>
2023-05-13 02:26:39 +0800

    动态表格

--- src/views/oauth2/login.vue
+++ src/views/oauth2/login.vue
@@ -3,6 +3,7 @@
     <br>
     <br>
     <el-button type="primary" @click="doLogin()">点击登录</el-button>
+
 </template>
 
 <script>
@@ -14,15 +15,20 @@ export default {
         doLogin() {
             let authorizeUrlEncode = this.$route.query.authorizeUrl;
             let authorizeUrl = decode(authorizeUrlEncode)
-            this.$http.get('http://localhost:8001/oauth2/doLogin?name=sa&pwd=123456').then(response => {
-                console.log(authorizeUrl);
-                console.log(response.headers);
-                var x = document.cookie;
-                console.log(x);
-                console.log(x.substring(8));
-                document.cookie = "satoken=" + x.substring(8)+ "; Max-Age=2592000; Expires=Sun, 11 Jun 2024 20:32:06 +0800; Path=/; Domain=tims.com"
-                // window.location.href = authorizeUrl;
-            })
+            window.location.href = "http://auth2.tims.com:8001/oauth2/doLogin?name=sa&pwd=123456"
+                .concat("&authorizeUrl=").concat(this.$route.query.authorizeUrl);
+
+            // this.$http.get('http://localhost:8001/oauth2/doLogin?name=sa&pwd=123456',
+            //     {withCredentials: true}
+            // ).then(response => {
+            //     console.log(authorizeUrl);
+            //     console.log(response.headers['set-cookie']);
+            //     var x = document.cookie;
+            //     console.log(x);
+            //     console.log(x.substring(8));
+            //     document.cookie = "satoken=" + x.substring(8)+ "; Max-Age=2592000; Expires=Sun, 11 Jun 2024 20:32:06 +0800; Path=/; Domain=tims.com"
+            //     // window.location.href = authorizeUrl;
+            // })
         }
     }
 }
