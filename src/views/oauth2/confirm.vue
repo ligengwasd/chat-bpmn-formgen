7ead1a01d6efcf356f88443331be386aaca5e001
geng.li <geng.li@timschina.com>
2023-05-13 12:10:34 +0800

    动态表格

---
+++ src/views/oauth2/confirm.vue
@@ -0,0 +1,27 @@
+<template>
+  OAuth-Server授权页
+    <br>
+    <br>
+    <el-button type="primary" @click="doLogin()">点击登录</el-button>
+
+</template>
+
+<script>
+import { encode, decode } from 'js-base64';
+
+export default {
+    name: "confirm",
+    methods: {
+        doLogin() {
+            let authorizeUrlEncode = this.$route.query.authorizeUrl;
+            let authorizeUrl = decode(authorizeUrlEncode)
+            window.location.href = "http://auth2.tims.com:8001/oauth2/doLogin?name=sa&pwd=123456"
+                .concat("&authorizeUrl=").concat(this.$route.query.authorizeUrl);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
