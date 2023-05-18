36a71cc947b2ef6e8578ae22d81e1c6092784b4f
geng.li <geng.li@timschina.com>
2023-05-18 16:24:38 +0800

    动态表格

---
+++ src/views/amis/amis.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="box"></div>
+</template>
+
+<script>
+import 'amis/sdk/sdk.js';
+import 'amis/lib/themes/default.css';
+
+import {  onMounted } from "vue";
+console.log("ces ");
+const props= defineProps({
+    amisjson: {
+        type: Object,
+        required: true
+    }
+})
+
+onMounted(() => {
+    // @ts-ignore
+    var amis = amisRequire('amis/embed');
+    console.log(amis);
+
+    let amisScoped = amis.embed('#box', props.amisjson);
+})
+
+</script>
+
+<style scoped>
+
+</style>
